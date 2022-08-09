f7e744ff547a4ab9c2b26152453a6cb6ceed6c7c
Dylan Hays <twentygx@outlook.com>
2022-08-08 19:18:29 -0500

    commit tree 73258f2bf031b11f765a304ca44f2ae206e0c858

--- index.js
+++ index.js
@@ -9,6 +9,60 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(args) {
+    const employee = {
+        firstName: args[0],
+        familyName: args[1],
+        title: args[2],
+        payPerHour: args[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employee;
+}
+
+function createEmployeeRecords(arrs) {
+    const emps = [];
+    for (let arr of arrs) {
+        emps.push(createEmployeeRecord(arr));
+    }
+    return emps;
+}
+
+function createTimeInEvent(punchStr) {
+    let punchArr = punchStr.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: punchArr[0],
+        hour: parseInt(punchArr[1])
+    })
+    return this;
+}
+
+function createTimeOutEvent(punchStr) {
+    const punchArr = punchStr.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: punchArr[0],
+        hour: parseInt(punchArr[1])
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIns = this.timeInEvents.filter(x => x.date === date);
+    const timeOuts = this.timeOutEvents.filter(x => x.date === date);
+    let timeOnDay = 0;
+    for (let i = 0; i < timeIns.length; i++) {
+        timeOnDay += (timeOuts[i].hour/100 - timeIns[i].hour/100);
+    }
+    return timeOnDay;
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +75,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstNameString) {
+    return srcArray.find(x => x.firstName === firstNameString);
+}
+
+function calculatePayroll(emps) {
+    let totalPayroll = 0;
+    for (let emp of emps) {
+        totalPayroll += allWagesFor.call(emp);
+    }
+    return totalPayroll;
+}
